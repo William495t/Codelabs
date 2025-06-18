0bff3fca858b138de8bbfcb936776ed178997a6e
William495t <williamfrancootero@gmail.com>
2025-06-18 00:32:54 -0500

    Kub Basic

---
+++ Comunicacion Async RabbitMQ/productoserviceasync/src/main/java/com/example/productoserviceasync/ProductoserviceasyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.productoserviceasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoserviceasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoserviceasyncApplication.class, args);
+	}
+
+}
---
+++ Comunicacion Async RabbitMQ/productoserviceasync/src/test/java/com/example/productoserviceasync/ProductoserviceasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productoserviceasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoserviceasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
