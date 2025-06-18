f6b840faa6a34574eaa4f62783f2638d7a03fa27
William495t <williamfrancootero@gmail.com>
2025-06-17 22:23:18 -0500

    Codelab Async

---
+++ Comunicacion Async RabbitMQ/pedidoserviceasync/src/main/java/com/example/pedidoserviceasync/PedidoserviceasyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedidoserviceasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoserviceasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoserviceasyncApplication.class, args);
+	}
+
+}
---
+++ Comunicacion Async RabbitMQ/pedidoserviceasync/src/test/java/com/example/pedidoserviceasync/PedidoserviceasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pedidoserviceasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoserviceasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
